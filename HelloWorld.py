9e50184edfd39a8dee8164eabfcce1fcc08c1ca7
jfrausto7 <jaf25jaf@gmail.com>
2019-12-22 21:48:18 -0700

    Sixth Commit
    This one was a biggie: I started using functions. Pretty fun stuff

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,5 @@
-print("Hello World")
+def HelloWorld():
+    print("Hello World")
 
 #This is a comment. Wow! I know how to comment now.
 
@@ -8,3 +9,18 @@ So epic, that I can do this
 Why is Python so similar to Java
 Lol
 """
+#Call a function below
+HelloWorld()
+
+#Function for greeting
+def UserInput(name):
+    print("Hi " + name)
+
+UserInput("Jacob")
+
+#function for adding two integers
+def Add(num1, num2):
+    return(num1+num2)
+
+sum = Add(2,5)
+print(sum)
