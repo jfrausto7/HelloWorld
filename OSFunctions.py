f9304c84e1bce096f5a74e443f2902bcb0a5c609
jfrausto7 <jaf25jaf@gmail.com>
2020-01-07 19:40:41 -0700

    Update OSFunctions.py

--- OSFunctions.py
+++ OSFunctions.py
@@ -1,16 +1,19 @@
-"""
-This is a header block comment
+"""This is a header block comment
 This script has a bunch of OS functions and stuff i wanted to try out
 Lol
 """
-#import the docx module first
+
+"""Below is an example of document manipulation.
+It uses the docx module
+Remember to install the 'python-docx' one, not the other one.
+This was a pain to get to work.
+"""
+
+# import the docx module first
 import docx
 
-#now assign a variable for it
+# now assign a variable for it
 doc = docx.Document("./testdoc.docx")
 
 for para in doc.paragraphs:
     print(para.text)
-
-
-
