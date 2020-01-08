825cb318b519c4514b774dd26adc1583d60ff726
jfrausto7 <jaf25jaf@gmail.com>
2020-01-07 19:36:56 -0700

    Eleventh Commit
    Started working with OS functions and Document manipulation via the "python-docx" module

--- OSFunctions.py
+++ OSFunctions.py
@@ -1,3 +1,16 @@
 """
-nothing so far here for this file
+This is a header block comment
+This script has a bunch of OS functions and stuff i wanted to try out
+Lol
 """
+#import the docx module first
+import docx
+
+#now assign a variable for it
+doc = docx.Document("./testdoc.docx")
+
+for para in doc.paragraphs:
+    print(para.text)
+
+
+
