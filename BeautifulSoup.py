32f0b0d18430de56b662b5c76bde8f131461af4b
jfrausto7 <jaf25jaf@gmail.com>
2020-01-09 16:13:35 -0700

    Fifteenth Commit
    Learned about some basics with the 'requests' module and BeautifulSoup for Web Parsing/Data Scraping

---
+++ BeautifulSoup.py
@@ -0,0 +1,11 @@
+#the imports first of course
+import requests
+from bs4 import BeautifulSoup
+
+#get the data html
+data = requests.get("https://google.com")
+#now make the soup
+soup = BeautifulSoup(data.text, "html.parser")
+
+#find div with image in it
+soup.find_all("div", {"id":"lga"})
