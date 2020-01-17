9411c11cef71e2a38ecccc72b2a345016481692a
jfrausto7 <jaf25jaf@gmail.com>
2020-01-16 22:28:27 -0700

    Twenty Second Commit
    Began working with the Tkinter module in Python. Learned how to work with labels, buttons, and frames so far.

---
+++ GUI_tkinter.py
@@ -0,0 +1,26 @@
+# imports
+from tkinter import *
+
+# init root
+root = Tk()
+
+# frames
+topFrame = Frame(root)
+topFrame.pack()
+botFrame = Frame(root)
+botFrame.pack(side = BOTTOM)
+
+# labels
+theLabel = Label(root, text="This is our Tkinter window")
+theLabel.pack()
+labelTwo = Label(root, text="This is our second sentence")
+labelTwo.pack()
+
+# buttons
+theButton = Button(None, text="Click Here", fg="blue")
+theButton.pack(fill = X)
+buttonTwo = Button(None, text="Hello!", fg="red")
+buttonTwo.pack(side=RIGHT, fill = Y)
+# mainloop function
+root.mainloop()
+
