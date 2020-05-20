4aef57f9a3f408a6814e79f0cee2a0d64e2722bf
jfrausto7 <jaf25jaf@gmail.com>
2020-05-20 01:02:38 -0600

    Update addZ.py

--- addZ.py
+++ addZ.py
@@ -6,3 +6,7 @@ from functools import reduce
 sentence = ['this','is','a','sentence']
 out_str = str(reduce(lambda x,y: x+"z "+y, sentence))
 print(out_str)
+
+s = [-3,-6,3,5,0,-5]
+
+print([x for x in s if x<0])
